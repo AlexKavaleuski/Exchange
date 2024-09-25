2e17fbae0727be4f8b9fe57478fea045f190d0b9
Alex Kavaleuski <alexey.kavaleuski@vexcash.com>
2024-09-25 17:23:48 +0300

    main init

---
+++ Exchange/HardcodedCurrencyProvider.cs
@@ -0,0 +1,33 @@
+﻿using Exchange.Enums;
+using Exchange.Interfaces;
+using Exchange.Models;
+
+namespace Exchange;
+
+public class HardcodedCurrencyProvider : ICurrencyProvider
+{
+    private readonly Dictionary<Currency, decimal> dkkRates = new()
+    {
+        { Currency.EUR, 743.94m },
+        { Currency.USD, 663.11m },
+        { Currency.GBP, 852.85m },
+        { Currency.SEK, 76.10m },
+        { Currency.NOK, 78.40m },
+        { Currency.CHF, 683.58m },
+        { Currency.JPY, 5.9740m }
+    };
+
+    public decimal GetExchangeRate(CurrencyPair pair)
+    {
+        if (pair.MainCurrency == pair.MoneyCurrency)
+            return 1.0m;
+
+        if (pair.MoneyCurrency == Currency.DKK)
+            return dkkRates[pair.MainCurrency] / 100;
+
+        if (pair.MainCurrency == Currency.DKK)
+            return 100 / dkkRates[pair.MoneyCurrency];
+
+        throw new NotSupportedException($"Exchange rate for {pair.MainCurrency}/{pair.MoneyCurrency} not supported.");
+    }
+}
---
+++ Exchange/Program.cs
@@ -0,0 +1,22 @@
+﻿using Exchange;
+
+try
+{
+    Console.WriteLine("Hello, World!");
+
+    var input = Console.ReadLine();
+
+    var currencyProvider = new HardcodedCurrencyProvider();
+    var currencyConverter = new CurrencyConverter(currencyProvider);
+    var exchangeService = new ExchangeService(currencyConverter);
+
+    var result = exchangeService.Convert(input);
+
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.ReadLine();
+}
